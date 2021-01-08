c92b821242402779aded1adcecf53c98cdd33611
Kiyoshiro Kawanabe <s19C1031UL@s.chibakoudai.jp>
2021-01-09 01:46:45 +0900

    add a file

--- scripts/n_prime.py
+++ scripts/n_prime.py
@@ -3,16 +3,19 @@ import rospy
 from std_msgs.msg import Int32
 
 def cb(message):
-    num = message.data
+  num = message.data
 
-    t = 0
-    for i in range(1, num):
-        if num % i == 0:
-            t+=1
-    if t == 1:
-        print(num)
+  t = 0
+  for i in range(1, num):
+    if num % i == 0:
+      t+=1
+    
+  if t == 1:
+    print(num,'\tは素数です')
+  else:
+    print(num,'\tは違います')
 
 if __name__ == '__main__':
-    rospy.init_node('twice')
-    sub = rospy.Subscriber('count_up', Int32, cb)
-    rospy.spin()
+  rospy.init_node('prime')
+  sub = rospy.Subscriber('rand', Int32, cb)
+  rospy.spin()
---
+++ scripts/rand.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import rospy
+import random
+from std_msgs.msg import Int32
+
+rospy.init_node('rand')
+pub = rospy.Publisher('rand', Int32, queue_size = 1)
+rate = rospy.Rate(1)
+
+while not rospy.is_shutdown():
+  n = random.randint(0, 200)
+  pub.publish(n)
+  rate.sleep()
